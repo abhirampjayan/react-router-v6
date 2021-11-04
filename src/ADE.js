0fe9f45d6b7c5878b5227a0a54b7a81c82fcbc1f
abhirampjayan <abhirampjayan@outlook.com>
2021-11-04 23:18:46 +0530

    first commit

---
+++ src/ADE.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+function ADE() {
+  return (
+    <div>
+      <h1>About</h1>
+      <Routes>
+        
+      </Routes>
+    </div>
+  );
+}
+
+export default ADE;
